09169775ca6662f6b932202898135c95b0794f80
Sazzad <mshossen75@gmail.com>
2023-02-19 01:47:20 +0600

    added Leave Application admin view

--- appointment/admin.py
+++ appointment/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # models
 from appointment.models import Appointment
+from appointment.models import AppointmentApplication
 
 # Register your models here.
 @admin.register(Appointment)
@@ -9,4 +10,10 @@ class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('appointment_of','appointment_date','meet_from','meet_to')
     search_fields = ('appointment_of','appointment_date')
     list_filter = ('is_active',)
+    list_per_page = 50
+
+@admin.register(AppointmentApplication)
+class AppointmentApplicationAdmin(admin.ModelAdmin):
+    list_display = ('request_by','issued_by','appointment_id','request_date')
+    search_fields = ('request_by','appointment_id')
     list_per_page = 50
