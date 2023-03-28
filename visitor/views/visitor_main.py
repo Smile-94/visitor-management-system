82e41795a703724baa23a98d2a51710ae26c2951
Sazzad Hossen <mshossen75@gmail.com>
2023-03-29 04:34:17 +0600

    modified appointment in visitor

--- visitor/views/visitor_main.py
+++ visitor/views/visitor_main.py
@@ -4,24 +4,28 @@ from django.shortcuts import render
 
 # Permission Classes
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.mixins import UserPassesTestMixin
+from visitor.permission import VisitorPassesTestMixin
 
 # Django class based view
 from django.views import View
+from django.views.generic import TemplateView
+
+# Models
+from appointment.models import AppointmentApplication
 
 
 
 # Create your views here.
-class VisitorHomeView(LoginRequiredMixin, UserPassesTestMixin, View):
+class VisitorHomeView(LoginRequiredMixin, VisitorPassesTestMixin, TemplateView):
     template_name = 'visitor/index.html'
 
-    # User pass test to view the visitor home view
-    def test_func(self):
-        return self.request.user.is_visitor
-
-    def get(self, request, *args, **kwargs):
-
-        context_data = {
-            'title': 'Visitor Home'
-        }
-        return render(request, self.template_name, context=context_data)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Visitor Home"
+        context["pending_appointment"] = AppointmentApplication.objects.filter(request_by=self.request.user, accept_status=False, decline_status=False).count() 
+        context["accpted_appointment"] = AppointmentApplication.objects.filter(request_by=self.request.user, accept_status=True, decline_status=False, cancel_status=True).count() 
+        context["declined_appointment"] = AppointmentApplication.objects.filter(request_by=self.request.user, accept_status=False, decline_status=True, cancel_status=False).count() 
+        context["cancel_appointment"] = AppointmentApplication.objects.filter(request_by=self.request.user, accept_status=True, decline_status=False, cancel_status=True).count() 
+        return context
+    
