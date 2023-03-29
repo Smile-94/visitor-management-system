35ba29ab9172ff7cde5ae3afda3478403076d443
Sazzad Hossen <mshossen75@gmail.com>
2023-03-29 22:28:05 +0600

    Modified Authority

--- authority/views/authority_main.py
+++ authority/views/authority_main.py
@@ -8,6 +8,11 @@ from authority.permission import AdminPassesTestMixin
 # class Based View
 from django.views.generic import TemplateView
 
+# Models
+from employee.models import EmployeeInfo
+from visitor.models import VisitorInfo
+from appointment.models import AppointmentApplication
+
 
 
 # Create your views here.
@@ -19,5 +24,9 @@ class AdminHomeView(LoginRequiredMixin, AdminPassesTestMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Admin Panel"
+        context["total_employee"] = EmployeeInfo.objects.all().count()
+        context["total_visitor"] = VisitorInfo.objects.all().count()
+        context["total_appointment"] = VisitorInfo.objects.all().count()
+        context["total_appointment"] = AppointmentApplication.objects.all().count()
         return context
 
