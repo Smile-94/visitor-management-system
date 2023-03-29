dc53ec496603b3fe05c743e53fcb522754d55193
Sazzad Hossen <mshossen75@gmail.com>
2023-03-29 22:06:31 +0600

    Added On Arival Appoint pdf report

--- report/urls.py
+++ report/urls.py
@@ -7,5 +7,6 @@ from report.views import generate_pdf
 urlpatterns = [
     path('visitor-profile/<int:pk>/', generate_pdf.VisittorProvilePdfView.as_view(), name='visitor_profile'),
     path('employee-profile/<int:pk>/', generate_pdf.EmployeeProfileView.as_view(), name='employee_profile'),
-    path('appointment-report/<int:pk>/', generate_pdf.AppointmentDetailsPdf.as_view(), name='appointment_report')
+    path('appointment-report/<int:pk>/', generate_pdf.AppointmentDetailsPdf.as_view(), name='appointment_report'),
+    path('onarival-appointment-report/<int:pk>/', generate_pdf.OnArivalAppointmentPdf.as_view(), name='onarivla_appointment_report'),
 ]
